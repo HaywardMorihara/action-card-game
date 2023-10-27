3a009054808cc5cf3b32cd7523a606a395fa321c
HaywardMorihara <nathanielmorihara@gmail.com>
2023-10-27 16:15:57 -0400

    Stubbing CardGame APIs

--- card_game/CardGame.gd
+++ card_game/CardGame.gd
@@ -7,9 +7,10 @@ var _animation_queue : Array[Callable] = []
 var _is_card_animation_in_progress : bool = false
 
 func _ready():
-	GlobalSignals.draw_cards.connect(_draw_cards)
+	# TODO Is this needed? Should it be here?
+#	GlobalSignals.draw_cards.connect(draw_cards)
 	# TODO Remove - for testing
-	_draw_cards(5);
+	draw_cards(5);
 
 func _physics_process(delta):
 	while not _animation_queue.is_empty() and not _is_card_animation_in_progress:
@@ -20,9 +21,104 @@ func _physics_process(delta):
 func _on_animation_finished():
 	_is_card_animation_in_progress = false;
 
-func _draw_cards(count : int):
+# APIs
+
+# Deck APIs
+
+func get_deck_contents() -> Array[Card]:
+	# TODO
+	return []
+
+func draw_cards(count : int):
 	for i in count:
 		_draw_card();
+
+func move_from_deck_to_hand(card : Card) -> Card:
+	# TODO
+	return null
+
+func discard_from_top_of_deck(count : int):
+	# TODO
+	pass
+
+func discard_from_deck(card : Card) -> bool:
+	# TODO
+	return false
+
+func shuffle_deck():
+	# TODO
+	pass
+
+# Remove from Deck
+func remove_from_deck(card : Card) -> Card:
+	# TODO 
+	return null
+
+
+# Hand APIs
+
+func move_from_hand_to_top_of_deck(card : Card):
+	# TODO
+	pass
+
+func move_from_hand_to_bottom_of_deck(card : Card):
+	# TODO 
+	pass
+
+# Hand to Discard (specific, random)
+func discard_from_hand(card : Card):
+	# TODO 
+	pass
+	
+func discard_random_from_hand(count : int) -> Array[Card]:
+	# TODO
+	return []
+
+func discard_all_from_hand() -> Array[Card]:
+	# TODO
+	return []
+
+func remove_from_hand(card : Card):
+	# TODO
+	pass
+
+func remove_random_from_hand(count : int) -> Array[Card]:
+	# TODO 
+	return []
+
+
+# Discard PIle APIs
+
+func get_discard_pile_contents() -> Array[Card]:
+	# TODO
+	return []
+
+func move_from_discard_to_hand(card : Card):
+	# TODO
+	pass
+
+func move_from_discard_to_top_of_deck(card : Card):
+	# TODO
+	pass
+
+func move_from_discard_to_bottom_of_deck(card : Card):
+	# TODO
+	pass
+
+func move_all_from_discard_to_top_of_deck():
+	# TODO
+	pass
+
+func move_all_from_discard_to_bottom_of_deck():
+	# TODO
+	pass
+
+func remove_from_discard(card : Card):
+	# TODO 
+	pass
+
+
+# Private Functions
 
 func _draw_card():
 	var next_card : Card = deck.draw_card();
