728a45471fbf7923f7a160ea2d26c8756566b994
HaywardMorihara <nathanielmorihara@gmail.com>
2023-11-21 20:48:03 -0500

    Save Button offset on completion

--- world/objects/interactives/Playmat.gd
+++ world/objects/interactives/Playmat.gd
@@ -6,6 +6,9 @@ class_name Playmat extends Interactive
 @onready var options : Control = $PlaymatOptions as Control;
 @onready var save_button : Button = $PlaymatOptions/SaveButton as Button;
 @onready var default_save_text := save_button.text;
+@onready var default_save_button_pos := save_button.position as Vector2;
+
+var save_button_complete_pos_offset : Vector2 = Vector2(8, 0);
 
 func _ready() -> void:
 	assert(playmat_id, "playmat_id must be set");
@@ -18,6 +21,7 @@ func _exit_interaction() -> void:
 	options.hide();
 	save_button.text = default_save_text;
 	save_button.disabled = false;
+	save_button.position = default_save_button_pos;
 
 func _on_shuffle_button_pressed() -> void:
 	GlobalSignals.reshuffle.emit();
@@ -27,8 +31,9 @@ func _on_save_button_pressed() -> void:
 	GlobalState.save();
 	
 func _on_save_complete() -> void:
-	save_button.text = "Save Complete!";
+	save_button.text = "Saved!";
 	save_button.disabled = true;
+	save_button.position += save_button_complete_pos_offset;
 
 func _on_edit_deck_button_pressed() -> void:
 	GlobalState.last_playmat_data.playmat_id = playmat_id;
