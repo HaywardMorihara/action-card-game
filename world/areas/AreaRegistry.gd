641a03807fa6763afed26886c98c004546b54f73
HaywardMorihara <nathanielmorihara@gmail.com>
2024-01-03 19:53:13 -0500

    Started the Christmas Area, layed down initial tiles

--- world/areas/AreaRegistry.gd
+++ world/areas/AreaRegistry.gd
@@ -4,6 +4,7 @@ class_name AreaRegistry extends Node
 # Note: It is possible (and should be) to have multiple area_id's pointing at the same scene
 const area_id_to_scene_path := {
 	"demo_area": "res://world/areas/DemoArea.tscn",
+	"christmas_area": "res://world/areas/ChristmasArea.tscn",
 	"library": "res://world/buildings/library/LibraryArea.tscn",
 	"house_1": "res://world/buildings/house_1/House1Area.tscn"
 }
