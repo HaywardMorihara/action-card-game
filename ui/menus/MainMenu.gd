71d5384d13e9c5413d7add7d190ab320a16f5806
HaywardMorihara <nathanielmorihara@gmail.com>
2024-03-23 20:03:21 -0400

    Main Menu Keyboard Navigation

--- ui/menus/MainMenu.gd
+++ ui/menus/MainMenu.gd
@@ -2,14 +2,17 @@ extends Control
 
 const main_scene_path : String = "res://Main.tscn"
 
+@onready var new_game_button : Button = $NewGameButton as Button;
 @onready var continue_button : Button = $ContinueButton as Button;
 
 func _ready() -> void:
 	GlobalState.reset();
 	if GlobalState.save_data_exists():
 		continue_button.disabled = false;
+		continue_button.grab_focus();
 	else:
 		continue_button.disabled = true;
+		new_game_button.grab_focus();
 
 func _on_new_game_button_pressed() -> void:
 	GlobalState.reset();
